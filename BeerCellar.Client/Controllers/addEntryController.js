27fa9f0dc7547a4a9b4a3277d82a0834e5fab0b8
Dave Pollot <dpollot@gmail.com>
2013-05-23 09:31:11 -0400

    Adds - Gatorade me, bitch!

--- BeerCellar.Client/Controllers/addEntryController.js
+++ BeerCellar.Client/Controllers/addEntryController.js
@@ -1,7 +1,14 @@
 ﻿
 var addEntryController = function addEntryController($scope, $http, $location) {
+    $scope.entry = { beerName: "", breweryName: "", count: "", year: "" };
     $scope.add = function () {
-        alert("Added!");
-        $location.path("/");
+
+        var json = JSON.stringify($scope.entry);
+        $http.post('http://localhost:49167/api/beercellar', json).success(function () {
+            alert("Added!");
+            $location.path("/");
+        }).error(function (data, status, headers, config) {
+            alert(data);
+        });
     };
 };
--- BeerCellar.Service/Controllers/BeerCellarController.cs
+++ BeerCellar.Service/Controllers/BeerCellarController.cs
@@ -1,4 +1,5 @@
 ﻿using BeerCellar.Core;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,11 @@ namespace BeerCellar.Service.Controllers
     public class BeerCellarController : ApiController
     {
         #region actions
-        // GET api/beercellar
+        /// <summary>
+        /// GET api/beercellar
+        /// Returns the collection of cellar entries
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<CellarEntry> Get()
         {
             var repo = new BeerCellar.Data.BeerCellarRepository();
@@ -20,25 +25,47 @@ namespace BeerCellar.Service.Controllers
             return collection;
         }
 
-        // GET api/beercellar/5
+        /// <summary>
+        /// GET api/beercellar/5
+        /// Returns the entry with the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string Get(int id)
         {
-            return "value";
+            throw new NotImplementedException("I ain't been implemented yo.  Gatorade me bitch!");
         }
 
-        // POST api/beercellar
-        public void Post([FromBody]string value)
+        /// <summary>
+        /// POST api/beercellar
+        /// Inserts the given entry
+        /// </summary>
+        /// <param name="value">the json representation of a CellarEntry</param>
+        public void Post([FromBody]CellarEntry entry)
         {
+            var repo = new BeerCellar.Data.BeerCellarRepository();
+            repo.InsertEntry(entry);
         }
 
-        // PUT api/beercellar/5
+        /// <summary>
+        /// PUT api/beercellar/5
+        /// updates the entry with the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         public void Put(int id, [FromBody]string value)
         {
+            throw new NotImplementedException("I ain't been implemented yo.  Gatorade me bitch!");
         }
 
-        // DELETE api/beercellar/5
+        /// <summary>
+        /// DELETE api/beercellar/5
+        /// deletes the entry with the given id
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
+            throw new NotImplementedException("I ain't been implemented yo.  Gatorade me bitch!");
         }
         #endregion
     }
